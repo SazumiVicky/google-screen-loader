a50147fa82ff19f0c7ae5771146ea8bf49ae88b2
SazumiVicky <75471215+SazumiVicky@users.noreply.github.com>
2022-02-05 08:44:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+<script src="https://code.jquery.com/jquery-3.6.0.js"
+integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaqq30Dh27Xc0jk="
+crossorigin="anonymous"></script>
+
+<script type='text'Javascript'>
+    $(window).load(function() {
+        $(".card").remove();
+    });
+</script>
